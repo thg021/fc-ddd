44685a811e92cf33a52bb932a319804eda318653
Thiago Silva <itm_tsilva@uolinc.com>
2022-09-18 19:41:09 -0300

    feat: repository Order
    adicionado order repository

--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -1,33 +1,50 @@
-import OrderItem from './order-item';
-export class Order{
+import OrderItem from "./order-item";
+export class Order {
   private _id: string;
   private _customerId: string;
-  private _orderItems: OrderItem[];
+  private _items: OrderItem[];
+  private _total: number;
 
-  constructor(id: string, customerId: string, orderItems: OrderItem[]){
-    this._id = id
-    this._customerId = customerId
-    this._orderItems = orderItems
-    this.validate()
+  constructor(id: string, customerId: string, items: OrderItem[]) {
+    this._id = id;
+    this._customerId = customerId;
+    this._items = items;
+    this._total = this.total();
+    this.validate();
   }
 
-  validate(): boolean{
-    if(this._id.length === 0){
-      throw new Error('Id is required')
-    }
-
-    if(this._customerId.length === 0){
-      throw new Error('CustomerId is required')
-    }
-
-    if(this._orderItems.length === 0){
-      throw new Error("Items are required")
-    }
-
-    return true
+  get id(): string {
+    return this._id;
   }
 
-  total(){
-    return this._orderItems.reduce((acc, item) => acc + item.price, 0)
+  get customerId(): string {
+    return this._customerId;
+  }
+
+  get items(): OrderItem[] {
+    return this._items;
+  }
+
+  validate(): boolean {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    if (this._customerId.length === 0) {
+      throw new Error("CustomerId is required");
+    }
+    if (this._items.length === 0) {
+      throw new Error("Items are required");
+    }
+
+    if (this._items.some((item) => item.quantity <= 0)) {
+      throw new Error("Quantity must be greater than 0");
+    }
+
+    return true;
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.price, 0);
   }
 }
+
